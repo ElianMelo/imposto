4173148fb91971b16372cbf093ddecde41cd3ac1
ElianMelo <elianmelomorais@gmail.com>
2020-05-15 00:37:31 -0300

    Consolidade de renda

--- src/imposto/Programa.java
+++ src/imposto/Programa.java
@@ -8,7 +8,8 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double sal, serv, cap, med, edu;
+		double sal, serv, cap, med, edu, impSal, impServ, impCap;
+		impSal = impServ = impCap = 0.0;
 		
 		System.out.print("Renda anual com salário: ");
 		sal = sc.nextDouble();
@@ -20,6 +21,30 @@ public class Programa {
 		med = sc.nextDouble();
 		System.out.print("Gastos educacionais: ");
 		edu = sc.nextDouble();
+		
+		if(sal < 3000) {
+			impSal = 0.0;
+		} else if (sal < 5000) {
+			impSal = sal * 0.1;
+		} else {
+			impSal = sal * 0.2;
+		}
+		
+		if(serv > 0) {
+			impServ = serv * 0.15;
+		}
+		
+		if(cap > 0) {
+			impCap = cap * 0.2;
+		}
+		
+		System.out.println("");
+		System.out.println("RELATÓRIO DE IMPOSTO DE RENDA");
+		System.out.println("");
+		System.out.println("CONSOLIDADO DE RENDA:");
+		System.out.printf("Imposto sobre salário: %.2f%n", impSal);
+		System.out.printf("Imposto sobre serviços: %.2f%n", impServ);
+		System.out.printf("Imposto sobre ganho de capital: %.2f%n", impCap);
 		
 		sc.close();
 	}
