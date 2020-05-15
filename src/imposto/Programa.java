fddcb578e2039722203b8402250df47d6761fb4d
ElianMelo <elianmelomorais@gmail.com>
2020-05-15 00:25:43 -0300

    Leitura dos dados

--- src/imposto/Programa.java
+++ src/imposto/Programa.java
@@ -8,6 +8,19 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		double sal, serv, cap, med, edu;
+		
+		System.out.print("Renda anual com salário: ");
+		sal = sc.nextDouble();
+		System.out.print("Renda anual com prestação de serviço: ");
+		serv = sc.nextDouble();
+		System.out.print("Renda anual com ganho de capital: ");
+		cap = sc.nextDouble();
+		System.out.print("Gastos médicos: ");
+		med = sc.nextDouble();
+		System.out.print("Gastos educacionais: ");
+		edu = sc.nextDouble();
+		
 		sc.close();
 	}
 }
